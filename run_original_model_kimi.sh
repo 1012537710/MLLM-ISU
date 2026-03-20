caecf08eaefbdd991014cd422c94eca60248d82b
1012537710 <116879109+1012537710@users.noreply.github.com>
2026-03-20 16:28:29 +0800

    Add files via upload

---
+++ run_original_model_kimi.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+####################
+### 加载依赖模块环境 ###
+#####################
+
+# module load compilers/cuda/11.7
+# module load nccl/2.18.3-1_cuda12.1
+# module load compilers/gcc/12.2.0
+# module load cudnn/8.9.5.29_cuda12.x
+# module load tensorboard/2.11.2
+# module load miniforge3/24.1
+# module list
+# conda list
+# 正确激活conda环境
+source $(conda info --base)/etc/profile.d/conda.sh  # source ~/miniconda3/etc/profile.d/conda.sh
+conda activate kimi
+python /home/bingxing2/ailab/scxlab0016/yepeng/predict_kimivl.py
